dbe6b004e9afea8784bbc9f4a9c872fd98c82585
aboulayetchakoura <aboulayet63@gmail.com>
2026-06-09 17:46:51 -0400

    done styling contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,13 +1,13 @@
 // import { Navbar } from "./Navbar"
 
-import { FaPhone, FaEnvelope, FaLinkedin,FaGithub, FaTwitter, FaFacebook, FaPen } from "react-icons/fa6"
+import { FaPhone, FaEnvelope, FaLinkedin,FaGithub, FaTwitter, FaFacebook, FaPen, FaInstagram} from "react-icons/fa6"
 
 
 export const Contact =()=>{
     return (<>
    {/* <Navbar/> */}
-   <section className="min-h-screen scroll-mt-24" id='contact'>
-    <div>
+   <section className="min-h-screen scroll-mt-24 flex flex-col items-center justify-center" id='contact'>
+   
      <div>
         <h1 className="text-3xl font-bold text-center mb-1">Contact Me</h1>
         <p className="text-center text-gray-400 mb-5  max-w-2xl mx-auto">
@@ -16,46 +16,51 @@ export const Contact =()=>{
           potential clients, and anyone interested in my work.
         </p>
      </div>
-     <div className="flex items-center justify-center gap-10">
-     </div>
-        <div className="flex items-center justify-center gap-10">  
-            
+     
+       
+            <div className="flex items-center justify-center gap-4">
+              <div className="flex flex-col items-start  mb-0">
 
-                <a href="tel:347-518-2522"> <span className="text-2xl font-bold text-blue-500"> <FaPhone /> Phone :</span>
-                 <h1 >347-518-2522</h1>
+              <span className="text-lg font-bold text-blue-500 flex"> 
+                <FaPhone   className="text-lg font-bold text-blue-500 mr-2" /> Phone :
+                </span>
+                 <h1 className="text-xl  hover:text-blue-500 transition duration-500">347-518-2522</h1>
+              </div>
+              <div className="flex flex-col items-start mb-0">
+
+                <span className="text-2xl font-bold text-blue-500 flex"> 
+                <FaEnvelope className="text-lg font-bold text-blue-500 mr-2"/>Email :
+                </span>
+                 <h1 className="text-xl  hover:text-blue-500 transition duration-500">aboulayet63@gmail.com</h1>
+              </div>
+           
+                 <div className="flex flex-col items-start ">
+                <span className="text-lg font-bold text-blue-500 flex"> 
+                    <FaPen className="text-2xl font-bold text-blue-500 "/>Address :</span>
+                <h1 className="text-xl  hover:text-blue-500 transition duration-500">New York, USA</h1>
+
+                 </div>
+            </div>
+             <div className="flex items-center gap-4 justify-center mt-8">
+                <a href="https://github.com/aboulayet63" target="_blank" rel="noopener noreferrer">
+                <FaGithub className="text-3xl hover:text-blue-500 transition duration-500 "/>
+                    
                 </a>
-
-                <a href="mailto:ab@gmail.com"> <span className="text-2xl font-bold text-blue-500"> <FaEnvelope /> :</span>
-                 <h1 >aboulayet63@gmail.com</h1>
+                <a href="https://twitter.com/aboulayet63" target="_blank" rel="noopener noreferrer">
+                    <FaTwitter className="text-3xl hover:text-blue-500 transition duration-500"/>
                 </a>
-                <FaPen className="text-2xl font-bold text-blue-500"/>
-
-            
-            <a href="https://github.com/aboulayet63" target="_blank" rel="noopener noreferrer">
-                <span className="text-2xl font-bold text-blue-500"><FaGithub/>:</span>
-                <h1 >aboulayet63</h1>
-            </a>
-            <a href="https://www.aboulayetchakoura.com" target="_blank" rel="noopener noreferrer">
-                <span className="text-2xl font-bold text-blue-500">🌐:</span>
-                <h1 >www.aboulayetchakoura.com</h1>
-            </a>    
-            <a href="https://www.aboulayetchakoura.com/resume.pdf" target="_blank" rel="noopener noreferrer">
-                <span className="text-2xl font-bold text-blue-500">📄:</span>
-                <h1 >Resume</h1>
-            </a>    
-              <a href="https://twitter.com/aboulayet63" target="_blank" rel="noopener noreferrer">
-                <span className="text-2xl font-bold text-blue-500"><FaTwitter/>:</span>
-            </a>
-
-            <a href="https://www.facebook.com/aboulayet63" target="_blank" rel="noopener noreferrer">
-                <span className="text-2xl font-bold text-blue-500"><FaFacebook/>:</span>
-              
-            </a>
-            <a href="https://www.linkedin.com/in/aboulaye-tchakoura-9b1a4b1b3/" target="_blank" rel="noopener noreferrer">
-            <FaLinkedin/>
-            </a>
-        </div>
-    </div>
+                <a href="https://www.facebook.com/aboulayet63" target="_blank" rel="noopener noreferrer">
+                <FaFacebook className="text-3xl hover:text-blue-500 transition duration-500"/>  
+                </a>
+                <a href="https://www.linkedin.com/in/aboulaye-tchakoura-9b1a4b1b3/" target="_blank" rel="noopener noreferrer">
+                <FaLinkedin className="text-3xl hover:text-blue-500 transition duration-500"/>
+                </a>
+                    <a href="https://www.instagram.com/aboulayet63/" target="_blank" rel="noopener noreferrer"> 
+                <FaInstagram className="text-3xl hover:text-blue-500 transition duration-500"/>
+                </a>
+             </div>
+    
+  
     
    </section>
         </>)
